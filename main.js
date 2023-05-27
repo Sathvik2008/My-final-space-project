de8a0c304131de4cfd83d002aef7d87240eb7232
Sathvik2008 <109099815+Sathvik2008@users.noreply.github.com>
2023-05-27 13:30:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function suninfo()
+{
+    document.getElementById("suninfo").style.display="block";       
+}
+
+function mercuryinfo()
+{
+    document.getElementById("mercuryinfo").style.display="block";       
+}
+
+function venusinfo()
+{
+    document.getElementById("venusinfo").style.display="block";       
+}
+
+function earthinfo()
+{
+    document.getElementById("earthinfo").style.display="block";       
+}
+
+function marsinfo()
+{
+    document.getElementById("marsinfo").style.display="block";       
+}
+function jupiterinfo()
+{
+    document.getElementById("jupiterinfo").style.display="block";       
+}
+
+function saturninfo()
+{
+    document.getElementById("saturninfo").style.display="block";       
+}
+
+function uranusinfo()
+{
+    document.getElementById("uranusinfo").style.display="block";       
+}
+
+function neptuneinfo()
+{
+    document.getElementById("neptuneinfo").style.display="block";       
+}
